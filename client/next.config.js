9801e89ba11c28eead632e676c87d503f669eddd
Ayush2623 <93155287+Ayush2623@users.noreply.github.com>
2024-05-01 00:56:54 +0530

    cmt

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig}*/
 const nextConfig = {
     images: {
-        domains:['res.cloudinary.com','randomuser.me'],
+        domains:['res.cloudinary.com'],
       },
-      experimental:{
-        reactRoot: true,
-        suppressHydrationWarning: true,
-      }
 }
 
 module.exports = nextConfig
